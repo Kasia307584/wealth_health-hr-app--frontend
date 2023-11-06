ae1cff359a429410c5a5681632727e5511abc107
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-11-06 18:23:09 +0100

    react-bootstrap-table-next sample component added with dependencies updated

--- src/pages/list/ListView.jsx
+++ src/pages/list/ListView.jsx
@@ -1,13 +1,30 @@
 import { Link } from "react-router-dom";
+import BootstrapTable from "react-bootstrap-table-next";
 
 export default function List() {
+  const products = [{ id: 1, name: "Milk", price: 10 }];
+  const columns = [
+    {
+      dataField: "id",
+      text: "Product ID",
+    },
+    {
+      dataField: "name",
+      text: "Product Name",
+    },
+    {
+      dataField: "price",
+      text: "Product Price",
+    },
+  ];
+
   return (
     <>
       <div id="employee-div" className="container">
         <h1>Current Employees</h1>
-        <table id="employee-table" className="display"></table>
         <Link to="/">Home</Link>
       </div>
+      <BootstrapTable keyField="id" data={products} columns={columns} />
     </>
   );
 }
