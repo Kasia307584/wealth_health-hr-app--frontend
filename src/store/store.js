32910cc61ffc811abac53129084605769af168d1
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-11-08 16:43:29 +0100

    Redux's store initialised

---
+++ src/store/store.js
@@ -0,0 +1,39 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const userDataSlice = createSlice({
+  name: "userData",
+  initialState: {
+    firstName: "",
+    lastName: "",
+    startDate: "",
+    departement: "",
+    dateOfBirth: "",
+    street: "",
+    city: "",
+    state: "",
+    zipCode: "",
+    id: "",
+  },
+  reducers: {
+    setData: (state, action) => {
+      console.log(action.payload);
+      state.firstName = action.payload.firstName;
+      state.lastName = action.payload.lastName;
+      state.startDate = action.payload.startDate;
+      state.departement = action.payload.departement;
+      state.dateOfBirth = action.payload.dateOfBirth;
+      state.street = action.payload.street;
+      state.city = action.payload.city;
+      state.state = action.payload.state;
+      state.zipCode = action.payload.zipCode;
+      state.id = action.payload.id;
+    },
+  },
+});
+const store = configureStore({
+  reducer: userDataSlice.reducer,
+});
+
+const { setData } = userDataSlice.actions;
+
+export { setData, store };
