aba9800ac1af58831b2dde8966b9aad60b2a01a8
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-11-13 21:17:07 +0100

    my select component - first version finalized

--- src/mySelectComponent.jsx
+++ src/mySelectComponent.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 export default function KatarzynaSelect({ options, label, onChange }) {
-  console.log(options);
   const [selectedValue, setSelectValue] = useState("");
 
   useEffect(() => {
-    // const selectedOption = options.filter(option => (option.value === selectedValue))
-    // let selectedObj = {}
-    // options.forEach(option => {
-    //     if(option.value === selectedOption) {
-    //         selectedObj = option
-    //     }
-    // })
-    onChange(selectedValue);
+    const selectedOption = options.filter(
+      (option) => option.value === selectedValue
+    );
+    onChange(selectedOption[0]);
   }, [selectedValue]);
 
   function capitalize(word) {
@@ -22,12 +17,12 @@ export default function KatarzynaSelect({ options, label, onChange }) {
 
   return (
     <>
-      {label && <label htmlFor="department">{capitalize(label)}</label>}
+      {label && <label htmlFor={label}>{capitalize(label)}</label>}
       <select
         aria-label={`select ${label}`}
         className="form-select"
-        id="department"
-        name="department"
+        id={label}
+        name={label}
         value={selectedValue}
         onChange={(e) => setSelectValue(e.target.value)}
       >
