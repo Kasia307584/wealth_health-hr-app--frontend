a76ee30f6a07bdbb45c1668feb908812c2c377f6
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2023-11-23 17:27:43 +0100

    Data table functionalities and basic styling added, data and columns passed as props

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -2,104 +2,125 @@ import {
   useReactTable,
   getCoreRowModel,
   flexRender,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
 } from "@tanstack/react-table";
-import { store } from "../store/store";
-import mockData from "../constants/MOCK_DATA";
-// import { useSelector } from "react-redux";
+import { useState } from "react";
 
-export default function Table() {
-  const data = store.getState().employeeTable;
-  // const selectorTest = useSelector((state) => state.employeeTable);
-
-  const columns = [
-    {
-      header: "First Name",
-      accessorKey: "firstName",
-    },
-    {
-      header: "Last Name",
-      accessorKey: "lastName",
-    },
-    {
-      header: "Start Date",
-      accessorKey: "startDate",
-    },
-    {
-      header: "Department",
-      accessorKey: "department",
-    },
-    {
-      header: "Date of Birth",
-      accessorKey: "dateOfBirth",
-    },
-    {
-      header: "Street",
-      accessorKey: "street",
-    },
-    {
-      header: "City",
-      accessorKey: "city",
-    },
-    {
-      header: "State",
-      accessorKey: "state",
-    },
-    {
-      header: "Zip Code",
-      accessorKey: "zipCode",
-    },
-  ];
+export default function Table({ data, columns }) {
+  const [sorting, setSorting] = useState([]);
+  const [filtering, setFiltering] = useState("");
 
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    state: {
+      sorting: sorting,
+      globalFilter: filtering,
+    },
+    onSortingChange: setSorting,
+    onGlobalFilterChange: setFiltering,
   });
 
   return (
     <div>
-      <table>
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => {
-            return (
-              <tr key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <th key={header.id}>
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                  </th>
+      <div>
+        <span>
+          Show
+          <select
+            value={table.getState().pagination.pageSize}
+            onChange={(e) => {
+              table.setPageSize(Number(e.target.value));
+            }}
+          >
+            {[10, 25, 50, 100].map((pageSize) => (
+              <option key={pageSize} value={pageSize}>
+                {pageSize}
+              </option>
+            ))}
+          </select>
+          entries
+        </span>
+        <span>
+          Search:
+          <input
+            type="text"
+            value={filtering}
+            onChange={(e) => setFiltering(e.target.value)}
+          />
+        </span>
+      </div>
+      <div className="w3-container">
+        <table className="w3-table-all">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => {
+              return (
+                <tr key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => (
+                    <th
+                      key={header.id}
+                      onClick={header.column.getToggleSortingHandler()}
+                    >
+                      {flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                      {
+                        { asc: "🔼", desc: "🔽" }[
+                          header.column.getIsSorted() ?? null
+                        ]
+                      }
+                    </th>
+                  ))}
+                </tr>
+              );
+            })}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
                 ))}
               </tr>
-            );
-          })}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-          {mockData.map((row) => (
-            <tr key={row.id}>
-              <td>{row.firstName}</td>
-              <td>{row.lastName}</td>
-              <td>{row.startDate}</td>
-              <td>{row.department}</td>
-              <td>{row.dateOfBirth}</td>
-              <td>{row.street}</td>
-              <td>{row.city}</td>
-              <td>{row.state}</td>
-              <td>{row.zipCode}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            ))}
+          </tbody>
+        </table>
+        <div>
+          <span>
+            Page
+            {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </span>
+          <span>
+            <button onClick={() => table.setPageIndex(0)}>First</button>
+            <button
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </button>
+            <button
+              onClick={() => table.setPageIndex(table.getPageCount() - 1)}
+            >
+              Last
+            </button>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- src/pages/ListView.jsx
+++ src/pages/ListView.jsx
@@ -1,12 +1,54 @@
 import { Link } from "react-router-dom";
 import Table from "../components/DataTable";
+import { store } from "../store/store";
 
 export default function List() {
+  const data = store.getState().employeeTable;
+
+  const columns = [
+    {
+      header: "First Name",
+      accessorKey: "firstName",
+    },
+    {
+      header: "Last Name",
+      accessorKey: "lastName",
+    },
+    {
+      header: "Start Date",
+      accessorKey: "startDate",
+    },
+    {
+      header: "Department",
+      accessorKey: "department",
+    },
+    {
+      header: "Date of Birth",
+      accessorKey: "dateOfBirth",
+    },
+    {
+      header: "Street",
+      accessorKey: "street",
+    },
+    {
+      header: "City",
+      accessorKey: "city",
+    },
+    {
+      header: "State",
+      accessorKey: "state",
+    },
+    {
+      header: "Zip Code",
+      accessorKey: "zipCode",
+    },
+  ];
+
   return (
     <>
       <div id="employee-div" className="container">
         <h1>Current Employees</h1>
-        <Table />
+        <Table data={data} columns={columns} />
         <Link to="/">Home</Link>
       </div>
     </>
